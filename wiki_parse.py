0d5429dcc22417f800bcdc5fbfaa18b263407fbe
Jakub Svec <jakubsvec001@gmail.com>
2018-10-27 13:13:04 -0700

    BEGIN CONVERT save to MongoDB, not JSON

--- wiki_parse.py
+++ wiki_parse.py
@@ -6,25 +6,23 @@ import mwparserfromhell
 import re
 import json
 from timeit import default_timer as timer
+from multiprocessing import Pool 
+import tqdm 
+from itertools import chain
+from functools import partial
+import pandas as pd
+import glob
+from pymongo import MongoClient
 
-
-# re_math = re.compile(r'<math([> ].*?)(</math>|/>)', re.DOTALL|re.UNICODE)
-# re_all_other_tags = re.compile(r'<(.*?)>', re.DOTALL|re.UNICODE)
-# re_categories = re.compile(r'\[\[Category:[^][]*\]\]', re.UNICODE)
-# re_rm_file_image_temp = re.compile(r'\[\[([fF]ile:|[iI]mage)[^]]*(\]\])', re.UNICODE)
-# re_rm_url_keep_description = re.compile(r'\[(\w+):\/\/(.*?)(( (.*?))|())\]', re.UNICODE)
-# re_simplify_links = re.compile(r'\[([^][]*)\|([^][]*)\]', re.DOTALL|re.UNICODE)
-# re_keep_image_description = re.compile(r'\n\[\[[iI]mage(.*?)(\|.*?)*\|(.*?)\]\]', re.UNICODE)
-# re_infobox = re.compile(r'(?=\{Infobox)(\{([^{}]|(?1))*\})', re.UNICODE)
-# re_remove_url = re.compile(r'\[(\w+):\/\/(.*?)(( (.*?))|())\]', re.UNICODE)
-
-def get_lines_bz2(filename): 
+def get_lines_bz2(filename, limit=None): 
     """yield each uncompressed line from bz2 file"""
-    for line in subprocess.Popen(['bzcat'], 
+    for i, line in enumerate(subprocess.Popen(['bzcat'], 
                              stdin = open(filename, 'rb'), 
                              stdout = subprocess.PIPE
-                             ).stdout:
+                             ).stdout):
         yield line.decode()
+        if limit and i >= limit:
+            break
 
 def find_pages(lines, input_titles, limit=None):
     """yield each page from a wikidump"""
@@ -39,8 +37,8 @@ def find_pages(lines, input_titles, limit=None):
         elif line.startswith('</page>'):
             search_count += 1
             inpage = False
-            text = ''.join(page)
-            parsed = parse_page(text, input_titles)
+            raw_xml = ''.join(page)
+            parsed = parse_page(raw_xml, input_titles)
             if parsed:
                 found_count += 1
                 yield parsed
@@ -52,17 +50,26 @@ def find_pages(lines, input_titles, limit=None):
         elif inpage:
             page.append(line)
 
+target = 'math'
 
-def parse_page(page, input_titles):
+def parse_page(raw_xml, input_titles):
     """Return a dict of page content"""
-    soup = bs(page, 'lxml')
+    global target
+    soup = bs(raw_xml, 'lxml')
     title = soup.select_one('title').text
-    page_content = soup.select_one('text').text
-    timestamp = soup.select_one('timestamp').text
     if title in input_titles:
-        wiki = mwparserfromhell.parse(page_content)
-        cleaned_text = strip_stripped_code(wiki) 
-        tags = wiki.filter_tags()
+        id = soup.select_one('id').text
+        markup_text = soup.select_one('text').text
+        timestamp = soup.select_one('timestamp').text
+        revision = soup.select_one('revision').text
+        wiki = mwparserfromhell.parse(markup_text)
+        cleaned_text = []
+        kw = ('Category:', 'thumb')
+        for line in wiki.split('\n'):
+            if line.startswith(kw):
+                continue
+            cleaned_text.append(line) 
+
         wikilinks = []
         for link in wiki.filter_wikilinks():
             link = link.strip('[]')
@@ -72,31 +79,51 @@ def parse_page(page, input_titles):
                 'en:' not in link and \
                 'Image:' not in link:
                 wikilinks.append(link)
-        return {'title': title, 
-                'stripped_content': cleaned_text, 
-                'full_content': page_content,
-                'wikilinks': wikilinks,
-                # 'tags': tags,
-                'timestamp': timestamp}
+
+        return {
+            'title': title,
+            'revision': revision,
+            'timestamp': timestamp ,
+            'id': id, 
+            'raw_xml': raw_xml,
+            'markup_text': ''.join(markup_text),
+            'cleaned_text': ' '.join(cleaned_text),
+            'links': wikilinks,
+            'target': target,
+        }
 
 
-def create_corpus(file, input_titles, save=True, limit=None):
-    '''Return a list of articles in a dictionary format'''
+input_titles = pd.read_csv('small_seed_data/math_articles_8687', sep='\t', encoding='utf-8', header=None)[0].tolist()
+
+def create_corpus(filein, dir_out='temp_results', input_titles=input_titles, save=True, limit=None):
+    '''Return a list of articles in a dictionary format OR
+       save articles to a mongodb database'''
     start = timer()
-    lines = get_lines_bz2(file)
+    name_str = filein.partition('-')[-1].split('.')[-3]
+    lines = get_lines_bz2(filein)
     pages = find_pages(lines, input_titles, limit)
-    if save:
-        fout = open('data/temp_results/file1.json', 'w')
-        save_json(fout, pages)
-        fout.close()
-    else:
-        pages = list(pages)
+    # CONTIUNE HERE TO SAVE INTO MONGODB
+    # if save:
+    #     mc = MongoClient()
+    #     db = mc['cache']
+    #     math = db['math']
+    #     for page in pages:
+    #         cached_article = math.find_one({'title': page['title']})
+    #         if cashed_article is None:
+    #             math.insert_one(page)
+    # else:
+    #     pages = list(pages)
+    #     return pages
     end = timer()
     time = round((end - start) / 60)
     stopwatch = f'It took {time} minutes to complete the search'
-    return stopwatch, pages
+    print(' SAVED TO: ' + dir_out + name_str + '.json')
+    return stopwatch
+
 
 def strip_stripped_code(wiki):
+    """use mwparserfromhell to strip code, then remove lines
+    that start with 'Category' or 'thumb'."""
     stripped = wiki.strip_code()
     cleaned = []
     kw = ('Category:', 'thumb')
@@ -106,26 +133,48 @@ def strip_stripped_code(wiki):
         cleaned.append(line)
     return cleaned    
         
-# if __name__ == '__main__':
+
+def multi_process_corpus(dump_file, title_file):
+    start = timer()
+    global input_titles
+    input_titles = title_file
+    dump_list = glob.glob(dump_file + '*.bz2')
+    input_titles = pd.read_csv(title_file, sep='\t', encoding='utf-8', header=None)[0].tolist()
+    pool = Pool(processes = os.cpu_count())
+
+    # Map (service, tasks), applies function to each partition
+    pool.map(create_corpus, dump_list[:4])
+
+    pool.close()
+    pool.join()
+
+    end = timer()
+    stopwatch = round((start - end)/60, 2) 
+    print(f'{stopwatch} seconds elapsed.')
+
+
+if __name__ == '__main__':
     
-#     if len(sys.argv) != 2:
-#         print('Usage: python check_wiki_corpus.py <corpus_file>')
-#         sys.exit(1)
+    start = timer()
+    n = 8
+    dumps = glob.glob(str(sys.argv[1]) + '*.bz2')
+    input_titles = sys.argv[2]
+    target = sys.argv[3]
 
-#     corpus_file = open(sys.argv[1],'r')
-#     check_corpus(corpus_file)
-#     corpus = load_corpus(corpus_file)
+    input_titles = pd.read_csv(str(input_titles), sep='\t', encoding='utf-8', header=None)[0].tolist()
+    if len(sys.argv) != 4:
+        print(f"Usage: python wiki_parse.py <dump directory> <target article file> <topic target name>\nArgs given: {len(sys.argv)}")
+        sys.exit(1)
     
+    pool = Pool(processes = os.cpu_count()) 
 
-# soup = bs(b'\n'.join(lines).decode('utf-8'), 'lxml')
+    # Map (service, tasks), applies function to each partition
+    results = pool.map(create_corpus, dumps[:n])
 
-def save_json(fout, pages):
-    for page in pages:
-        fout.write(json.dumps(page) + '\n')
+    pool.close()
+    pool.join()
 
-def load_json(filename):
-    data = []
-    with open('data/temp_results/file1.json', 'r') as fin:
-        for l in fin.readlines():
-            data.append(json.loads(l))
-    return data
+    end = timer()
+    stopwatch = round(((start - end) / 60), 2)
+    print(f'{stopwatch} seconds elapsed.')
+    
