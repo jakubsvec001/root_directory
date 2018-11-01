6a42beeba22d23a2adf466046c786371c3282ed2
Jakub Svec <jakubsvec001@gmail.com>
2018-10-31 18:55:40 -0700

    Small progress

--- piwikibot_search.py
+++ piwikibot_search.py
@@ -1,179 +1,26 @@
 
 # Mathematics (20306 nodes) 
-# Only English article math articles (786 / 20306 nodes)
 """
-     SELECT DISTINCT ?item ?article ?sitelink ?linkTo WHERE {
-     { ?item wdt:P361* wd:Q395. }
-     UNION
-     { ?item (wdt:P361/wdt:P279*) wd:Q395. }
-     UNION
-     { ?item (wdt:P31/wdt:P279*) wd:Q395. }
-     UNION
-     { ?item (wdt:P921/wdt:P279*) wd:Q395. }
-     ?sitelink ^schema:name ?article.
-     ?article schema:about ?item.
-     ?article schema:isPartOf <https://en.wikipedia.org/>.
-     OPTIONAL { ?item wdt:P361 ?linkTo. }
-     SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-        }
-     """
-
-# Math with english wikipedia entries, plus non-article nodes 
-        """
-        SELECT distinct ?item ?article ?sitelink ?linkTo WHERE {
-          { ?item wdt:P361* wd:Q395 .}
-          union
-          { ?item wdt:P361/wdt:P279* wd:Q395 .}
-          union
-          { ?item wdt:P31/wdt:P279* wd:Q1936384 .}
-          union
-          { ?item wdt:P921/wdt:P279* wd:Q395 .}
-          optional {?sitelink ^schema:name ?article .
-                    ?article schema:about ?item ;
-                    schema:isPartOf <https://en.wikipedia.org/> .
-                   }
-          OPTIONAL { ?item wdt:P361 ?linkTo. }
-          SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-        }
-        """
-
-# Computer Science (375 / 16,706 nodes)
-"""
-SELECT DISTINCT ?item ?article ?sitelink ?linkTo WHERE {
-  { ?item wdt:P361* wd:Q21198. }
-  UNION
-  { ?item (wdt:P361/wdt:P279*) wd:Q21198. }
-  UNION
-  { ?item (wdt:P31/wdt:P279*) wd:Q21198. }
-  UNION
-  { ?item (wdt:P921/wdt:P279*) wd:Q21198. }
-  ?sitelink ^schema:name ?article.
+SPARQL Query:
+SELECT distinct ?item ?itemLabel ?linkTo WHERE {
+  { ?item wdt:P361* wd:Q395 .}
+  union
+  { ?item wdt:P361/wdt:P279* wd:Q395 .}
+  union
+  { ?item wdt:P31/wdt:P279* wd:Q1936384 .}
+  union
+  { ?item wdt:P921/wdt:P279* wd:Q395 .}
+  
   ?article schema:about ?item.
-  ?article schema:isPartOf <https://en.wikipedia.org/>.
+  
   OPTIONAL { ?item wdt:P361 ?linkTo. }
   SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
 }
 """
 
-# Physics (402 / 1,882 nodes)
+# History (138,504 nodes)
 """
-SELECT DISTINCT ?item ?article ?sitelink ?linkTo WHERE {
-  { ?item wdt:P361* wd:Q413. }
-  UNION
-  { ?item (wdt:P361/wdt:P279*) wd:Q413. }
-  UNION
-  { ?item (wdt:P31/wdt:P279*) wd:Q413. }
-  UNION
-  { ?item (wdt:P921/wdt:P279*) wd:Q413. }
-  UNION
-  { ?item wdt:P361* wd:Q658544. }
-  UNION
-  { ?item (wdt:P361/wdt:P279*) wd:Q658544. }
-  UNION
-  { ?item (wdt:P31/wdt:P279*) wd:Q658544. }
-  UNION
-  { ?item (wdt:P921/wdt:P279*) wd:Q658544. }
-  ?sitelink ^schema:name ?article.
-  ?article schema:about ?item.
-  ?article schema:isPartOf <https://en.wikipedia.org/>.
-  OPTIONAL { ?item wdt:P361 ?linkTo. }
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-}
-"""
-
-# Biology (726 / 61,674 nodes)
-"""
-SELECT DISTINCT ?item ?article ?sitelink ?linkTo WHERE {
-  { ?item wdt:P361* wd:Q420. }
-  UNION
-  { ?item (wdt:P361/wdt:P279*) wd:Q420. }
-  UNION
-  { ?item (wdt:P31/wdt:P279*) wd:Q420. }
-  UNION
-  { ?item (wdt:P921/wdt:P279*) wd:Q420. }
-  ?sitelink ^schema:name ?article.
-  ?article schema:about ?item.
-  ?article schema:isPartOf <https://en.wikipedia.org/>.
-  OPTIONAL { ?item wdt:P361 ?linkTo. }
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-}
-"""
-
-# Chemistry (273 / 15,674 nodes)
-"""
-SELECT DISTINCT ?item ?article ?sitelink ?linkTo WHERE {
-  { ?item wdt:P361* wd:Q2329. }
-  UNION
-  { ?item (wdt:P361/wdt:P279*) wd:Q2329. }
-  UNION
-  { ?item (wdt:P31/wdt:P279*) wd:Q2329. }
-  UNION
-  { ?item (wdt:P921/wdt:P279*) wd:Q2329. }
-  ?sitelink ^schema:name ?article.
-  ?article schema:about ?item.
-  ?article schema:isPartOf <https://en.wikipedia.org/>.
-  OPTIONAL { ?item wdt:P361 ?linkTo. }
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-}
-"""
-
-# Machine Learning (26 / 5,035 nodes)
-"""
-SELECT DISTINCT ?item ?article ?sitelink ?linkTo WHERE {
-  { ?item wdt:P361* wd:Q2539. }
-  UNION
-  { ?item (wdt:P361/wdt:P279*) wd:Q2539. }
-  #UNION
-  #{ ?item (wdt:P31/wdt:P279*) wd:Q2539. }
-  UNION
-  { ?item (wdt:P921/wdt:P279*) wd:Q2539. }
-  ?sitelink ^schema:name ?article.
-  ?article schema:about ?item.
-  ?article schema:isPartOf <https://en.wikipedia.org/>.
-  OPTIONAL { ?item wdt:P361 ?linkTo. }
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-}
-"""
-
-# EN wiki Medicine (266 / 3526 total res)
-"""
-SELECT DISTINCT ?item ?article ?sitelink ?linkTo WHERE {
-  { ?item wdt:P361* wd:Q11190. }
-  UNION
-  { ?item (wdt:P361/wdt:P279*) wd:Q11190. }
-  UNION
-  { ?item (wdt:P31/wdt:P279*) wd:Q11190. }
-  UNION
-  { ?item (wdt:P921/wdt:P279*) wd:Q11190. }
-  ?sitelink ^schema:name ?article.
-  ?article schema:about ?item.
-  ?article schema:isPartOf <https://en.wikipedia.org/>.
-  OPTIONAL { ?item wdt:P361 ?linkTo. }
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-}
-"""
-
-# Business ()
-"""
-SELECT DISTINCT ?item ?article ?sitelink ?linkTo WHERE {
-  { ?item wdt:P361* wd:Q4830453. }
-  UNION
-  { ?item (wdt:P361/wdt:P279*) wd:Q4830453. }
-  UNION
-  { ?item (wdt:P31/wdt:P279*) wd:Q4830453. }
-  UNION
-  { ?item (wdt:P921/wdt:P279*) wd:Q4830453. }
-  ?sitelink ^schema:name ?article.
-  ?article schema:about ?item.
-  ?article schema:isPartOf <https://en.wikipedia.org/>.
-  OPTIONAL { ?item wdt:P361 ?linkTo. }
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-}
-"""
-
-# History (87884 / 138,504 nodes)
-"""
-SELECT DISTINCT ?item ?article ?sitelink ?linkTo WHERE {
+SELECT DISTINCT ?item ?article WHERE {
   { ?item wdt:P361* wd:Q309. }
   UNION
   { ?item (wdt:P361/wdt:P279*) wd:Q309. }
@@ -181,45 +28,79 @@ SELECT DISTINCT ?item ?article ?sitelink ?linkTo WHERE {
   { ?item (wdt:P31/wdt:P279*) wd:Q309. }
   UNION
   { ?item (wdt:P921/wdt:P279*) wd:Q309. }
-  ?sitelink ^schema:name ?article.
+
   ?article schema:about ?item.
-  ?article schema:isPartOf <https://en.wikipedia.org/>.
+
   OPTIONAL { ?item wdt:P361 ?linkTo. }
   SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
 }
 """
 
-# Philosophy (585 nodes)
+# Biology (61,674 nodes)
 """
-SELECT DISTINCT ?item ?article ?sitelink ?linkTo WHERE {
-  { ?item wdt:P361* wd:Q5891. }
+SELECT DISTINCT ?item ?itemLabel ?linkTo WHERE {
+  { ?item wdt:P361* wd:Q420. }
   UNION
-  { ?item (wdt:P361/wdt:P279*) wd:Q5891. }
+  { ?item (wdt:P361/wdt:P279*) wd:Q420. }
   UNION
-  { ?item (wdt:P31/wdt:P279*) wd:Q5891. }
+  { ?item (wdt:P31/wdt:P279*) wd:Q420. }
   UNION
-  { ?item (wdt:P921/wdt:P279*) wd:Q5891. }
-  ?sitelink ^schema:name ?article.
-  ?article schema:about ?item.
-  ?article schema:isPartOf <https://en.wikipedia.org/>.
+  { ?item (wdt:P921/wdt:P279*) wd:Q420. }
   OPTIONAL { ?item wdt:P361 ?linkTo. }
   SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
 }
 """
 
-# Arts (24,442 nodes)
+# Chemistry (15,674 nodes)
 """
-SELECT DISTINCT ?item ?article ?sitelink ?linkTo WHERE {
-  { ?item wdt:P361* wd:Q735. }
+SELECT DISTINCT ?item ?itemLabel ?linkTo WHERE {
+  { ?item wdt:P361* wd:Q2329. }
   UNION
-  { ?item (wdt:P361/wdt:P279*) wd:Q735. }
+  { ?item (wdt:P361/wdt:P279*) wd:Q2329. }
   UNION
-  { ?item (wdt:P31/wdt:P279*) wd:Q735. }
+  { ?item (wdt:P31/wdt:P279*) wd:Q2329. }
   UNION
-  { ?item (wdt:P921/wdt:P279*) wd:Q735. }
-  ?sitelink ^schema:name ?article.
-  ?article schema:about ?item.
-  ?article schema:isPartOf <https://en.wikipedia.org/>.
+  { ?item (wdt:P921/wdt:P279*) wd:Q2329. }
+  OPTIONAL { ?item wdt:P361 ?linkTo. }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+}
+"""
+
+# Machine Learning (5,035 nodes)
+"""
+SELECT DISTINCT ?item ?itemLabel ?linkTo WHERE {
+  { ?item wdt:P361* wd:Q2539. }
+  UNION
+  { ?item (wdt:P361/wdt:P279*) wd:Q2539. }
+  #UNION
+  #{ ?item (wdt:P31/wdt:P279*) wd:Q2539. }
+  UNION
+  { ?item (wdt:P921/wdt:P279*) wd:Q2539. }
+  OPTIONAL { ?item wdt:P361 ?linkTo. }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+}
+"""
+
+# Engineering / Applied Science (32,600 nodes)
+"""
+SELECT DISTINCT ?item ?itemLabel ?linkTo WHERE {
+  { ?item wdt:P361* wd:Q11023. }
+  UNION
+  { ?item (wdt:P361/wdt:P279*) wd:Q11023. }
+  UNION
+  { ?item (wdt:P31/wdt:P279*) wd:Q11023. }
+  UNION
+  { ?item (wdt:P921/wdt:P279*) wd:Q11023. }
+  UNION
+  { ?item (wdt:P921/wdt:P279*) wd:Q11023. }
+  UNION
+  { ?item (wdt:P361/wdt:P279*) wd:Q28797. }
+  UNION
+  { ?item (wdt:P31/wdt:P279*) wd:Q28797. }
+  UNION
+  { ?item (wdt:P921/wdt:P279*) wd:Q28797. }
+  UNION
+  { ?item (wdt:P921/wdt:P279*) wd:Q28797. }
   OPTIONAL { ?item wdt:P361 ?linkTo. }
   SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
 }
@@ -227,7 +108,7 @@ SELECT DISTINCT ?item ?article ?sitelink ?linkTo WHERE {
 
 # Academics / People (140,837 nodes)
 """
-SELECT ?person ?article WHERE {
+SELECT ?person WHERE {
   { ?person wdt:P106 wd:Q901. }
   UNION
   { ?person wdt:P106 wd:Q170790. }
@@ -245,12 +126,10 @@ SELECT ?person ?article WHERE {
   { ?person wdt:P106 wd:Q82594. }
   UNION
   { ?person wdt:P106 wd:Q201788. }
-  ?sitelink ^schema:name ?article.
-  ?article schema:about ?item.
-  ?article schema:isPartOf <https://en.wikipedia.org/>.
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
 }
 """
+
 
 
 
