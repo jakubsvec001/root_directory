259424c6e3cbfcdc6cd2fbc6f6b69f647dd248d4
Jakub Svec <jakubsvec001@gmail.com>
2018-11-03 21:51:49 -0700

    MAKE expand_all_categories() use csv reader line by line

--- wiki_scraper.py
+++ wiki_scraper.py
@@ -1,9 +1,12 @@
 import os
 import time
 import sys
+import csv
 import pandas as pd
 from selenium.webdriver import Firefox
 from timeit import default_timer
+from urllib.parse import unquote
+
 
 def get_expand_buttons(browser):
     """Return a list of expand buttons to click on."""
@@ -28,27 +31,24 @@ def expand_all_categories(browser, category, search_depth=3, save='csv', all_pag
         depth += 1
     if save == 'csv':
         start = default_timer()
-        lst = []
-        for a in browser.find_elements_by_xpath('.//a'):
-            lst.append(a.get_attribute('href'))
-        links = pd.DataFrame(lst)
-        links = links.iloc[7:-32,:]
         if all_pages:
-            links.to_csv(f'seed_data/all_pages_{category}_d{depth}_{len(links)}.csv', sep=',', encoding='utf-8', header=None, index=False)
+            with open(f'seed_pages/all_pages_{category}_d{depth}.csv', 'w') as out:
+                writer = csv.writer(out, lineterminator='\n')
+                for a in browser.find_elements_by_xpath('.//a'):
+                    writer.writerow([a.get_attribute('href')])
         else:
-            links.to_csv(f'seed_categories/cat_pages_{category}_d{depth}_{len(links)}.csv', sep=',', encoding='utf-8', header=None, index=False)
+            with open(f'seed_categories/cat_pages_{category}_d{depth}.csv', 'w') as out:
+                writer = csv.writer(out, lineterminator='\n')
+                for a in browser.find_elements_by_xpath('.//a'):
+                    writer.writerow([a.get_attribute('href')])
         end = default_timer()
-        end_time = default_timer()
-        print(str(round((end-start)/60, 2)) + f' minutes to save to csv {len(links)} links')
-        print(str(round((end_time-start_time)/60, 2)) + f' minutes to finish entire search')
-        
+        print(str(round((end-start)/60, 2)) + f' minutes to save to csv')
     if save == False:
         pass
-    
-    
+    end_time = default_timer()
+    print(str(round((end_time-start_time)/60, 2)) + f' minutes to finish search')
 
-
-def get_links(category, search_depth=3, save='csv'):
+def get_pages(category, search_depth=3, save='csv'):
     """get the links from wikipedia's hidden category tree finder"""
     browser = Firefox()
     time.sleep(0.5)
@@ -74,6 +74,10 @@ def only_open(category, search_depth=5):
     category = category.replace(' ','_')
     expand_all_categories(browser, category, search_depth, save=False, all_pages=False)
 
+def clean_url(url):
+    base_url = 'https://en.wikipedia.org/wiki/'
+    url = unquote(url).strip('')
+    return url[len(base_url):]
 
 # def main():
 #     """Open the page and expand all categories."""
