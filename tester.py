4d09c70bb3e895d929a0df287a527d6afe50d75e
Jakub Svec <jakubsvec001@gmail.com>
2018-11-10 08:59:36 -0800

    Begin creating a program for direct requests with Miles

---
+++ tester.py
@@ -0,0 +1,44 @@
+url = 'https://en.wikipedia.org/w/api.php'
+params = dict(action='categorytree',
+              format='json',
+              category='Aviation_by_city',
+              options='{"mode":0,"hideprefix":20,"showcount":true,"namespaces":false}',
+              uselang='en',
+              formatversion='2')
+ 
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Referer': 'https://en.wikipedia.org/wiki/Special:CategoryTree?target=Category%3AAviation&mode=categories&namespaces=&title=Special%3ACategoryTree',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Connection': 'keep',
+            'Cookie': 'WMF-Last-Access-Global=10-Nov-2018, WMF-Last-Access=10-Nov-2018, enwikiUserName=Jakubsvec01, GeoIP=US:WA:Seattle:47.60:-122.33:v4',
+            'TE': 'Trailers'
+            }
+
+
+
+
+
+
+url = 'https://en.wikipedia.org/w/api.php'
+params = dict(action='categorytree',
+              format='json',
+              category='Aviation_by_city',
+              options='{"mode":0,"hideprefix":20,"showcount":true,"namespaces":false}',
+              uselang='en',
+              formatversion='2')
+ 
+ headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Referer': 'https://en.wikipedia.org/wiki/Special:CategoryTree?target=Category%3AAviation&mode=categories&namespaces=&title=Special%3ACategoryTree',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Connection': 'keep',
+            
+            
+
+            
+             --compressed -H -H  -H 
+ -alive' -H 'Cookie: WMF-Last-Access-Global=10-Nov-2018; WMF-Last-Access=10-Nov-2018; enwikiU
+ serName=Jakubsvec01; GeoIP=US:WA:Seattle:47.60:-122.33:v4' -H 'TE: Trailers'
