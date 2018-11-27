08185789f0668a90eac709058347c27b8fc5f86f
Jakub Svec <jakubsvec001@gmail.com>
2018-11-26 17:07:54 -0800

    ADD extra cleaning functionality to link output

--- wiki_text_parser.py
+++ wiki_text_parser.py
@@ -31,7 +31,7 @@ def parse_update_database(db_name, collection_name):
     """parse and update a mongodb database with cleaned xml"""
     document_generator = mongodb_page_stream(db_name, collection_name)
     for document in document_generator:
-        yield parse_page(document)
+        yield parse_page(document['full_raw_xml'])
 
  # Find math content:
 re_math = re.compile(r'<math([> ].*?)(</math>|/>)', re.DOTALL|re.UNICODE)
@@ -53,10 +53,9 @@ re_file_description = re.compile(r'\n\[\[[fF]ile(.*?)(\|.*?)*\|(.*?)\]\]', re.UN
 re_external_links = re.compile(r'<nowiki([> ].*?)(</nowiki>|/>)', re.DOTALL|re.UNICODE)
 
     
-def parse_page(document):
+def parse_page(xml):
     """parse the xml of a mondodb document"""
     # grab the raw xml from the document
-    xml = document['full_raw_xml']
     # extract links from xml
     links = get_links(xml)
     # extract image text from xml
@@ -78,12 +77,10 @@ def parse_page(document):
     # extract headers
     headers = get_headers(soup.text)
     markup_text = soup.select_one('text').text
-    return headers
-    remove_markup = mwparserfromhell.parse(wikicorpus.remove_markup(markup_text)).strip_code()
-    remove_markup = remove_markup.replace('\n','')
-    strip_code = mwparserfromhell.parse(markup_text).strip_code()
-    return {'timestamp': timestamp, 'strip_code': strip_code, 'xml': xml, 'soup': soup.text, 'remove_markup': remove_markup, 'clean_links':cleaned_links, 'categories': {page_title: categories} ,}
-
+    text_remove_markup = wikicorpus.remove_markup(markup_text)
+    text_strip_code = mwparserfromhell.parse(text_remove_markup).strip_code()
+    clean_text = text_strip_code.replace('\n','')
+    return {'clean_text': clean_text,'timestamp': timestamp, 'headers': headers, 'clean_links':cleaned_links, 'parent_categories': {page_title: categories}}
     # math = re_math.findall(markup_text)
     return file_desc
     return links
@@ -95,9 +92,9 @@ def get_links(xml):
     clean_links = []
     for link in links:
         if '|' in link:
-            clean_links.append(link.partition('|')[0])
+            clean_links.append(link.partition('|')[0].replace('#', ' '))
         else: 
-            clean_links.append(link)
+            clean_links.append(link.replace('#',' '))
     return clean_links
 
 def clean_image_desc(xml):
@@ -122,10 +119,17 @@ def replace_categories_in_xml(xml, categories):
 def clean_links_list(links_list):
     clean_links = []
     for link in links_list:
+        link = link.replace('(disambiguation)', '')
         if 'File:' not in link and \
            'Category:' not in link and \
            'Wikipedia:' not in link and \
            'en:' not in link and \
+           'User:' not in link and \
+           'Template:' not in link and \
+           'User talk:' not in link and \
+           'Special:' not in link and \
+           'Project:' not in link and \
+           'WP:' not in link and \
            'Image:' not in link:
             clean_links.append(link)
     return clean_links
@@ -141,7 +145,6 @@ def get_headers(text):
 
 def update_document():
     pass
-
 
 def _identify_page(self, raw_xml):
     """Indentify whether or not article is in self.titles_to_find"""
