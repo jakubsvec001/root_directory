297c4e73cd3317df12acf600e967d364edc83f6c
Jakub Svec <jakubsvec001@gmail.com>
2018-11-28 10:21:45 -0800

    ADD n-gram functionality to model pipeline

--- model.py
+++ model.py
@@ -9,15 +9,6 @@ import pandas as pd
 import numpy as np  
 from sklearn.feature_extraction import stop_words
 
-# class MyCorpus(object):
-#     """a generator class for reading pregenerated nlp training data, line by line"""
-
-#     def __iter__(self):
-#         with open('seed/training_mathematics_text', 'r') as fin:
-#             for line in fin:
-#             # assume there's one document per line, tokens separated by whitespace
-#                 yield dictionary.doc2bow(line.lower().split())
-
 def save_nlp_train_data(db_name, collection_name, target, fileout_dir):
     """use a mongodb collection and a subsampled target subset to create a .txt file
     with one line per document"""
@@ -32,20 +23,26 @@ def save_nlp_train_data(db_name, collection_name, target, fileout_dir):
             if row != 'nan':
                 fout.write(row + '\n')
 
-def create_trained_dictionary(filein):
+def create_trained_dictionary(filein, n_grams):
     """Use gensim to create a streamed dictionary"""
     # dictionary = [line for line in open(filein, 'r')]
-    p = PorterStemmer()
-    s = SnowballStemmer('english')
-    dictionary = corpora.Dictionary([s.stem(word) for word in line.split()] for line in open(filein, 'r'))
-    dict_removed_extra = remove_extra_words(dictionary)
+    dictionary = corpora.Dictionary(_stem_and_ngramizer(line, n_grams) for line in open(filein, 'r'))
+    dict_removed_extra = _remove_extra_words(dictionary)
     dict_removed_extra.save('nlp_training_data/math_dictionary.dict')
     return dict_removed_extra
 
-def remove_extra_words(dictionary):
-    """remove stopwords and words that appear only once"""
-    stop_ids = [dictionary.token2id[stopword] for stopword in stop_words.ENGLISH_STOP_WORDS
-                if stopword in dictionary.token2id]
-    once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.items() if docfreq == 1]
-    dictionary.filter_tokens(stop_ids + once_ids)
+def _remove_extra_words(dictionary):
+    """remove words that appear only once"""
+    once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.items() if docfreq <= 2]
+    dictionary.filter_tokens(once_ids)
     return dictionary
+
+def _stem_and_ngramizer(line, n_grams):
+    """stem all the words, generate ngrams, and return 
+       a list of all stemmed words and ngram phrases"""
+    p = PorterStemmer()
+    s = SnowballStemmer('english')
+    stopped = [word for word in line.split() if word not in stop_words.ENGLISH_STOP_WORDS]
+    stems = [s.stem(word) for word in stopped] 
+    grams = [[' '.join(stems[i:i+n]) for i in range(len(stems)-n+1)] for n in range(1, n_grams + 1)]
+    return [item for sublist in grams for item in sublist]
