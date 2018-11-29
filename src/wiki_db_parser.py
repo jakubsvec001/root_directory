61f671f009bbc4c21ba8feb68bf8b6e886191d7e
Jakub Svec <jakubsvec001@gmail.com>
2018-11-29 15:14:14 -0800

    BUG FIX small

--- src/wiki_db_parser.py
+++ src/wiki_db_parser.py
@@ -10,6 +10,7 @@ from pymongo import MongoClient
 from gensim.corpora import wikicorpus
 import sys
 
+
 def disect_update_database(db_name, collection_name):
     """parse and update a mongodb database with cleaned xml"""
     mc = MongoClient()
@@ -19,14 +20,16 @@ def disect_update_database(db_name, collection_name):
     count = 0
     for document in document_generator:
         features = disect_page(document['title'], document['full_raw_xml'])
-        update_document(collection, features)
+        _update_document(collection, features)
         count +=1
-        sys.stdout.write('\r'+ f'YAAAAAAAS! Updated {count} documents')
+        sys.stdout.write('\r'+ f'YAAAAAAAS! Updated {count} documents: Title = {document["title"]}')
+
 
 def mongodb_page_stream(collection):
     """yield a new page from a mongodb collection"""
     document_generator = collection.find()
     return document_generator
+
 
 def disect_page(title, xml):
     """parse raw wikipedia xml"""
@@ -64,6 +67,7 @@ def disect_page(title, xml):
             'parent_categories': {title: categories},
             'feature_union': feature_union}
 
+
 def replace_multiple(main_string, to_be_replaced, new_string):
     """replace extra elements in a text string"""
     for elem in to_be_replaced :
@@ -71,16 +75,49 @@ def replace_multiple(main_string, to_be_replaced, new_string):
             main_string = main_string.replace(elem, new_string)
     return  main_string
 
-def update_document(collection, features):
-    #import pdb; pdb.set_trace()
-    # if features['title'] == 'Category:Combinatorics journals':
-    #     raise ValueError('found it')
+
+def _update_document(collection, features):
+    """Used by disect_update_database(). Updates a document in mongodb"""
     collection.update_one({'title': features['title']}, {'$set': 
                                                            {'feature_union': features['feature_union'],
                                                             'parent_categories': features['parent_categories']}})
 
+
+def num_target_col_db(db_name, collection_name, target_name):
+    """add a column to each document with a number and the target value
+       for indexing train/test split. Uses _check_target_presence_in_db()"""
+    mc = MongoClient()
+    db = mc[db_name]
+    collection = db[collection_name]
+    _check_target_presence_in_db(collection, target_name)
+    docs = collection.find()
+    for idx, doc in enumerate(docs):
+        _id = doc['_id']
+        if doc['target'] == target_name:
+            target = 1
+        else:
+            target = 0
+        collection.update({'_id': ObjectId(_id)}, {'$set' : 
+                                                            {'idx/target': (idx, target)}})
+
+
+def _check_target_presence_in_db(collection, target_name):
+    """check for presence of target in db 
+    before adding num_target column"""
+    docs = collection.find()
+    hit = False
+    for doc in docs:
+        if doc['target'] == target_name:
+            hit = True
+            break
+    if hit == False:
+        raise ValueError('OH NOOO! No targets saved. '+
+                         'Check your target_name parameter')
+
+
 def join_features(page_title, headers, clean_text, cleaned_links):
     return ' '.join([page_title] + [clean_text] + cleaned_links + headers)
+
 
 def get_links(xml):
     links = re_interlinkstext_link.findall(xml)
@@ -92,12 +129,14 @@ def get_links(xml):
             clean_links.append(link.replace('#',' '))
     return clean_links
 
+
 def clean_image_desc(xml):
     image_desc = re_image_description.findall(xml)
     if image_desc != []:
         image_desc = ' '.join(image_desc[0][2:])
         image_desc = wikicorpus.remove_markup(image_desc)
     return image_desc
+
 
 def clean_file_desc(xml):
     file_desc = re_file_description.findall(xml)
@@ -106,12 +145,14 @@ def clean_file_desc(xml):
         file_desc = wikicorpus.remove_markup(file_desc)
     return file_desc
 
+
 def replace_categories_in_xml(xml, categories):
     if categories == [] or xml == []:
         return xml
     for category in categories:
         cleaned_xml = xml.replace(category, ' ')
     return cleaned_xml
+
 
 def clean_links_list(links_list):
     clean_links = []
@@ -137,6 +178,7 @@ def clean_links_list(links_list):
                                 ' '))
     return scrubbed_links
 
+
 def get_headers(text):
     headers = []
     lines = text.split('\n')
@@ -150,7 +192,6 @@ def get_headers(text):
                                 ['\n', '(', ')', ',', ';', '[', ']', '"', ':'], 
                                 ' '))
     return clean_headers
-
 
 
 # Find math content:
