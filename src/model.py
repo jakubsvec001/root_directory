c1515512c004a05206dcab9cee3eda6860a6e2d1
Jakub Svec <jakubsvec001@gmail.com>
2018-11-29 12:00:18 -0800

    BUG FIXES, small

--- src/model.py
+++ src/model.py
@@ -1,7 +1,8 @@
-import wiki_text_parser as wtp 
+import src.wiki_db_parser as wdbp 
+import sys
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.naive_bayes import MultinomialNB
-import sys
+from bson.objectid import ObjectId
 from gensim import corpora, models
 from gensim.parsing.porter import PorterStemmer
 from nltk.stem.snowball import SnowballStemmer
@@ -12,28 +13,31 @@ import pandas as pd
 import numpy as np  
 from sklearn.feature_extraction import stop_words
 
-def save_nlp_train_data(db_name, collection_name, target, fileout_dir):
-    """use a mongodb collection and a subsampled target subset to create a .txt file
-    with one line per document"""
+
+def save_txt_nlp_train_data(db_name, collection_name, target, fileout_dir, subset):
+    """use a mongodb collection and a subsampled target subset percentage
+    to create a .txt file with one line per document"""
     mc = MongoClient()
     db = mc[db_name]
     col = db[collection_name]
     target_pages = col.find({'target':target})
     df = pd.DataFrame(list(target_pages))['feature_union']
-    subsampled_df = df.sample(frac=0.8, replace=False)
+    subsampled_df = df.sample(frac=subset, replace=False)
     with open(fileout_dir, 'w') as fout:
         for row in subsampled_df:
             if row != 'nan':
                 fout.write(row + '\n')
 
+
 def train_save_dictionary_corpus(filein, n_grams, target):
     """Use gensim to create a streamed dictionary"""
-    dictionary = corpora.Dictionary(list_grams(filein, n_grams))
+    dictionary = corpora.Dictionary(_list_grams(filein, n_grams))
     dictionary.filter_extremes(no_below=5, no_above=0.5, keep_n = 100000)
-    dictionary.save(f'../nlp_training_data/{target}_dictionary.dict')
-    corpus = [dictionary.doc2bow(word) for word in list_grams(filein, n_grams)]
-    corpora.MmCorpus.serialize(f'../nlp_training_data/{target}_corpus.mm', corpus)
+    dictionary.save(f'nlp_training_data/{target}_dictionary.dict')
+    corpus = [dictionary.doc2bow(word) for word in _list_grams(filein, n_grams)]
+    corpora.MmCorpus.serialize(f'nlp_training_data/{target}_corpus.mm', corpus)
     return dictionary, corpus
+
 
 def train_save_tfidf(filein, target):
     """input is a bow corpus saved as a tfidf file. The output is 
@@ -48,17 +52,34 @@ def train_save_tfidf(filein, target):
     tfidf_corpus = tfidf[corpus]
     return tfidf_corpus
 
+
+def train_multinomial_nb(db_name, collection_name, num_target_col):
+    mc = MongoClient()
+    db = mc[db_name]
+    collection = db[collection_name]
+    docs = collection.find()
+    items = []
+    for doc in docs:
+        items.append(doc[num_target_col])
+    sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2)
+    items = np.array(items)
+    return items
+    sss.get_n_splits(items.T[0], items.T[1])
+
+
 def _remove_extra_words(dictionary):
-    """remove words that appear only once"""
+    """DEPRICATED! remove words that appear only once"""
     once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.items() if docfreq <= 2]
     dictionary.filter_tokens(once_ids)
     return dictionary
 
-def list_grams(filein, n_grams):
+
+def _list_grams(filein, n_grams):
     '''for each document, yield a list of strings'''
     with open(filein, 'r') as fin:
         for line in fin:
             yield _stem_and_ngramizer(line, n_grams)
+
 
 def _stem_and_ngramizer(line, n_grams):
     """stem all the words, generate ngrams, and return 
@@ -69,6 +90,7 @@ def _stem_and_ngramizer(line, n_grams):
     stems = [s.stem(word) for word in stopped] 
     grams = [[' '.join(stems[i:i+n]) for i in range(len(stems)-n+1)] for n in range(1, n_grams + 1)]
     return [item for sublist in grams for item in sublist]
+
 
 def main(arg1, arg2):
     pass
